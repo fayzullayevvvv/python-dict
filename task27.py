ad91cf10aca580ce696187c0be24c871c3a18d46
fayzullayevvvv <ozodbekfayzullayev1220@gmail.com>
2026-01-06 18:47:57 +0500

    done

--- task27.py
+++ task27.py
@@ -1,38 +1,42 @@
 def phonebook_menu(phonebook: dict[str, str]) -> None:
-    print('1.Kontakt qo‘shish.')
-    print('2.Barcha kontaktlarni chiqarish.')
-    print('3.Ism bo‘yicha telefon qidirish.')
+    while True:
+        print('1.Kontakt qo‘shish.')
+        print('2.Barcha kontaktlarni chiqarish.')
+        print('3.Ism bo‘yicha telefon qidirish.')
+        print('4.Chiqish.')
 
-    choice = input('Tanlov: ')
+        choice = input('>').strip()
 
-    if choice == '1':
-        name = input('Ism: ').strip().capitalize()
-        phone = input('Telefon raqam: ').strip()
-        phonebook[name] = phone
+        if choice == '1':
+            name = input('Ism: ').strip()
+            phone = input('Telefon raqam: ').strip()
+            phonebook[name] = phone
 
-        return f'{name.capitalize()} qo\'shildi.'
-    
-    
-    elif choice == '2':
-        if not phonebook:
-            return 'Kontakt bo\'sh.'
-        else:
-            all_contacts = ''
+            print(f'{name.capitalize()} qo\'shildi.')
+        
+        
+        elif choice == '2':
+            if not phonebook:
+                return 'Kontakt bo\'sh.'
+            else:
+                all_contacts = ''
+                for name, phone in phonebook.items():
+                    all_contacts += f'{name.title()} | {phone.title()}' + '\n'
+
+                print(all_contacts)
+            
+            
+        elif choice == '3':
+            search = input('Ism: ').strip()
             for name, phone in phonebook.items():
-                all_contacts += f'{name.title()} | {phone.title()}' + '\n'
-
-            return all_contacts
+                if search.lower() in name.lower():
+                    print(name, phone)
         
+        elif choice == '4':
+            return 'Dastur tugadi.'
         
-    elif choice == '3':
-        name = input('Ism: ').strip().capitalize()
-        if name.title() in phonebook.keys():
-            return phonebook[name]
         else:
-            return 'Bunday ismli kontakt mavjud emas.'
-    
-    else:
-        return 'Siz boshqa son yoki belgi kiritdingiz. Iltimos 1 dan 3 gacha kiriting.'
+            print('Bunday menu mavjud emas.')
     
 phonebook = {
     'Ali':'+998991223434',
